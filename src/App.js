88780a1c901e5ad772d8930d4be6fb82395c0041
Ali <superguy@nobody.com>
2023-11-28 06:54:54 +0400

    Version 6 - Workedd on it... A lot...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import Note from './Note';
+import notes from './notes';
+import './styles.css';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div>
+      <header>
+        <h1>My Notes App</h1>
+      </header>
+      <main>
+        {notes.map((note, index) => (
+          <Note key={index} note={note} />
+        ))}
+      </main>
+      <footer>
+        <p>Footer Content</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+      title: "Note 1",
+      content: "Content of note 1",
+      createdDate: "2023-01-01",
+      image: "image1.jpg"
+    },
+    {
+      title: "Note 2",
+      content: "Content of note 2",
+      createdDate: "2023-01-02"
+    },
+    // Add more notes here
+  ];
+  
+  export default notes;
+  
